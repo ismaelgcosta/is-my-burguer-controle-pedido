b2a0d2aecd2761a24e7a999c0dab196af4a62fe1
Ismael G Costa <notmymail@notmymail.com>
2024-05-10 02:58:01 -0300

    subindo arquivos faltantes

---
+++ api-main-build/src/main/java/br/com/ismyburguer/RestTemplateConfig.java
@@ -0,0 +1,74 @@
+package br.com.ismyburguer;
+
+import com.netflix.discovery.shared.transport.EurekaHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.client5.http.ssl.TrustAllStrategy;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.configuration.SSLContextFactory;
+import org.springframework.cloud.configuration.TlsProperties;
+import org.springframework.cloud.netflix.eureka.http.EurekaClientHttpRequestFactorySupplier;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateDiscoveryClientOptionalArgs;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateEurekaHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+@EnableDiscoveryClient
+@Profile(value = {"dev", "production"})
+public class RestTemplateConfig {
+
+    @Value("eureka.client.serviceUrl.defaultZone")
+    private String serviceUrl;
+
+    @Bean
+    public EurekaHttpClient eurekaHttpClient() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        return new RestTemplateEurekaHttpClient(restTemplate(), serviceUrl);
+    }
+
+    @Bean
+    public RestTemplateDiscoveryClientOptionalArgs restTemplateDiscoveryClientOptionalArgs(TlsProperties tlsProperties,
+                                                                                           EurekaClientHttpRequestFactorySupplier eurekaClientHttpRequestFactorySupplier)
+            throws GeneralSecurityException, IOException {
+        RestTemplateDiscoveryClientOptionalArgs args = new RestTemplateDiscoveryClientOptionalArgs(
+                eurekaClientHttpRequestFactorySupplier);
+        SSLContextFactory factory = new SSLContextFactory(tlsProperties);
+        args.setSSLContext(factory.createSSLContext());
+        args.setHostnameVerifier(NoopHostnameVerifier.INSTANCE);
+        return args;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create()
+                        .setSSLSocketFactory(SSLConnectionSocketFactoryBuilder.create()
+                                .setSslContext(SSLContextBuilder.create()
+                                        .loadTrustMaterial(TrustAllStrategy.INSTANCE)
+                                        .build())
+                                .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                                .build())
+                        .build())
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpclient);
+        return new RestTemplate(requestFactory);
+    }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+# precisa começar com TF_VAR_
+variable "TF_VAR_MONGODB_CONTROLE_PEDIDO_PASSWORD" {
+  description = "The password for the mongodb database."
+  type        = string
+  sensitive   = true
+}
+
+# precisa começar com TF_VAR_
+variable "TF_VAR_MONGODB_CONTROLE_PEDIDO_USERNAME" {
+  description = "The username for the mongodb database."
+  type        = string
+  sensitive   = true
+}
+
+# precisa começar com TF_VAR_
+variable "TF_VAR_IMAGE_VERSION" {
+  description = "The number of the new image version."
+  type        = string
+}
+
+# precisa começar com TF_VAR_
+variable "TF_VAR_SERVICE_DISCOVERY_USERNAME" {
+  description = "The master username for sd admin."
+  type        = string
+  sensitive   = true
+}
+
+# precisa começar com TF_VAR_
+variable "TF_VAR_SERVICE_DISCOVERY_PASSWORD" {
+  description = "The master password for the sd admin."
+  type        = string
+  sensitive   = true
+}
