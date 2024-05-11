435d2f85586555d5968b5f7fdbb80cce17728476
Ismael G Costa <notmymail@notmymail.com>
2024-05-11 00:53:48 -0300

    aumentando cobertura

--- api-main-build/src/test/java/br/com/ismyburguer/ApplicationUnitTest.java
+++ api-main-build/src/test/java/br/com/ismyburguer/ApplicationUnitTest.java
@@ -2,20 +2,11 @@ package br.com.ismyburguer;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
-
 class ApplicationUnitTest {
 
     @Test
-    void deveDarErroAoCriarManualmente() {
-        try {
-            Application.main(new String[]{});
-            fail();
-        } catch (Exception e) {
-            assertNotNull(e.getMessage());
-        }
-
+    void deveCriarManualmente() {
+        Application.main(new String[]{});
     }
 
 }
