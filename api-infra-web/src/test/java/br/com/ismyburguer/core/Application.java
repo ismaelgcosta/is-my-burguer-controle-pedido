0a00e2788eadad14a82b84e5a61c6a6c71e1162f
Ismael G Costa <notmymail@notmymail.com>
2024-05-10 21:56:12 -0300

    fix jacoco

--- api-infra-web/src/test/java/br/com/ismyburguer/core/Application.java
+++ api-infra-web/src/test/java/br/com/ismyburguer/core/Application.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(TestSecurityConfig.class)
 public class Application {
 
     public static void main(String[] args) {
